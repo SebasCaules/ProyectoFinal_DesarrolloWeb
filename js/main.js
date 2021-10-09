e133932938ae2afe5ccd80da41eb038e2bf35c01
SebasCaules <sebastiancaules5@gmail.com>
2021-10-09 19:14:22 -0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+// Array de Productos
+
+
 let arrayProductos = [
   {
+    id : 0,
     img: "https://images-na.ssl-images-amazon.com/images/I/8145RpwlT1L._SX355_.jpg",
     nombre: "The Wall",
     banda: "Pink Floyd",
@@ -9,6 +13,7 @@ let arrayProductos = [
     fechaEntrada: [22, 08]
   },
   {
+    id : 1,
     img: "https://www.efeeme.com/wp-content/uploads/supertramp-16-07-13-a.jpg",
     nombre: "Breakfast in America",
     banda: "Supertramp",
@@ -19,6 +24,7 @@ let arrayProductos = [
     fechaEntrada: [22, 08]
   },
   {
+    id : 2,
     img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Supertramp_-_Even_in_the_Quietest_Moments.jpg/220px-Supertramp_-_Even_in_the_Quietest_Moments.jpg",
     nombre: "Even in the quitest moments",
     banda: "Supertramp",
@@ -28,6 +34,7 @@ let arrayProductos = [
     fechaEntrada: [22, 08]
   },
   {
+    id : 3,
     img: "https://4.bp.blogspot.com/-19fALnGXDVI/TYgGwRSAHhI/AAAAAAAAA8E/Hiz1dmdmkHI/s1600/hablando-a-tu-coraz%25C3%25B3n.jpg",
     nombre: "Tango",
     banda: "Charly Garcia, Pedro Aznar",
@@ -37,6 +44,7 @@ let arrayProductos = [
     fechaEntrada: [22, 08]
   },
   {
+    id : 4,
     img: "https://images.clarin.com/2013/08/17/SyfoZ-j3ml_720x0.jpg",
     nombre: "Clix Modernos",
     banda: "Charly Garcia",
@@ -46,6 +54,7 @@ let arrayProductos = [
     fechaEntrada: [22, 08]
   },
   {
+    id : 5,
     img: "https://i.pinimg.com/originals/32/ff/59/32ff596ad9f170b5651c3f1e0001fed1.jpg",
     nombre: "Peperina",
     banda: "Seru Giran",
@@ -55,6 +64,7 @@ let arrayProductos = [
     fechaEntrada: [22, 08]
   },
   {
+    id : 6,
     img: "https://images.genius.com/40ea18f3146c97a764d7e6099dcd7fc9.1000x1000x1.jpg",
     nombre: "La Grasa de las Capitales",
     banda: "Seru Giran",
@@ -64,6 +74,7 @@ let arrayProductos = [
     fechaEntrada: [22, 08]
   },
   {
+    id : 7,
     img: "https://img.discogs.com/1gcRIIOL3sxo_GfE_2hGZFvR_ok=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-6377375-1625425102-6846.jpeg.jpg",
     nombre: "Vida",
     banda: "Chico Buarque",
@@ -73,6 +84,7 @@ let arrayProductos = [
     fechaEntrada: [22, 08]
   },
   {
+    id : 8,
     img: "https://miro.medium.com/max/1400/0*MDgb-RBa1vBVNfrs.jpg",
     nombre: "Construcao",
     banda: "Chico Buarque",
@@ -82,29 +94,52 @@ let arrayProductos = [
     fechaEntrada: [22, 08]
   },
   {
+    id : 9,
     img: "https://http2.mlstatic.com/D_NQ_NP_683357-MLA46508378619_062021-W.jpg",
     nombre: "Bajo Belgrano",
     banda: "Spinetta Jade",
     precio: 3500,
     cantidadLPs: 1,
-    duracion: [0, 40],
+    duracion: [2, 1],
     fechaEntrada: [22, 08]
   },
 ];
 
+// Inicializador del Catalogo
 const contenedorProductos = document.getElementById("productos");
+
+
+
+
+function duracionCheck(producto) {
+  let duracionChangeResult = '';
+  let duracionChangeMin = '';
+
+  //Check Minutos
+  if (producto.duracion[1] == 1) {
+    duracionChangeMin = `${producto.duracion[1]} minuto`;
+  } else {
+    duracionChangeMin = `${producto.duracion[1]} minutos`;
+  };
+
+  //Check Horas
+  if (producto.duracion[0] == 0) {
+    return duracionChangeMin;
+  } else {
+  if (producto.duracion[0] == 1) {
+      duracionChangeResult = `${producto.duracion[0]} hora y ${duracionChangeMin}`;
+    } else {
+      duracionChangeResult = `${producto.duracion[0]} horas y ${duracionChangeMin}`;
+    };
+  };
+  return duracionChangeResult;
+}
 
 let idCounter = 0;
 
 arrayProductos.forEach((producto) => {
-
-  let duracionChange = String;
-
-  if (producto.duracion[0] == 0) {
-    duracionChange = `${producto.duracion[1]} minutos`
-  } else {
-    duracionChange = `${producto.duracion[0]} horas y ${producto.duracion[1]} minutos`
-  }
+  //Check de la duracion
+  let duracionChange = duracionCheck(producto);
 
   const div = document.createElement("div");
   div.className = "card-img-top card";
@@ -137,6 +172,8 @@ arrayProductos.forEach((producto) => {
   idCounter++;
 });
 
+
+//Generador de borde verde cuando el boton es clickeado
 const borderAdquierBtn = document.getElementById('addBtn');
 const borderAdquier = document.getElementById('cardsContainer');
 
@@ -144,6 +181,6 @@ const borderAdquier = document.getElementById('cardsContainer');
 borderAdquierBtn.addEventListener('click', () => {
   borderAdquier.classList.toggle('border')
   setTimeout(function(){
-    borderAdquier.classList.toggle('border');  
+    borderAdquier.classList.toggle('border');
   },500)
-},)
+})
