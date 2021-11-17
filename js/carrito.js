e7d67afbb2ad95fd791d99f5a2dac6422b32e2d9
SebasCaules <89231064+SebasCaules@users.noreply.github.com>
2021-11-17 16:54:26 -0300

    changes

--- js/carrito.js
+++ js/carrito.js
@@ -73,28 +73,6 @@ for (var i = 0; i <= productosInLSArray.length; i++) {
   }
 }
 
-/* <div>
-                      <img
-                      class="card-img-top"
-                      src="${producto[i].img}"
-                      alt="${producto[i].nombre} Imagen Portada"
-                      />
-                      <div class="card-body">
-                      <h5 class="card-title">${producto[i].nombre}</h5>
-                      <h6 class="card-title">${producto[i].banda}</h6>
-                      </div>
-                      <ul class="list-group list-group-flush">
-                      <li class="list-group-item">Fecha de Entrada : ${producto[i].fechaEntrada[0]} / ${producto[i].fechaEntrada[1]}</li>
-                      <li class="list-group-item">${producto[i].precio} $</li>
-                      <li class="list-group-item">Contiene ${producto[i].cantidadLPs} LP('s)</li>
-                      <li class="list-group-item">Cantidad de Items : ${productosInCarrito[i]}</li>
-                      </ul>
-                      <div class="card-body">
-                      <a href="#" class="card-link">Escuchar en Spotify</a>
-                      </div>
-                      <button class="btn addBtn" id="addBtn${producto.id}" type="submit">Añadir al Carrito</button>
-                  </div> */
-
 $("#deleteAll").on("click", () => {
   if (productosInLS) {
     localStorage.removeItem("productos");
